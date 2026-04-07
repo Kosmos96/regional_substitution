6053c812d591dc5a30b2173e4dac83542021f5bb
Kosmos96 <kosmic96@mail.ru>
2026-04-07 12:51:52 +0300

    Add module name and description in install lang file

---
+++ protobyte.cityseo/install/lang/ru/index.php
@@ -0,0 +1,7 @@
+<?php
+$MESS['PROTOBYTE_CITYSEO_MODULE_NAME'] = 'SEO-подмена контента по разделам и регионам';
+$MESS['PROTOBYTE_CITYSEO_MODULE_DESCRIPTION'] = 'Модуль для региональной подмены SEO-данных, заголовков и контента услуг на основе URL и HL-блоков. Позволяет автоматически подставлять H1, meta-теги, описание и дополнительный текст для каждого региона и раздела сайта.';
+$MESS['PROTOBYTE_CITYSEO_PARTNER_NAME'] = 'Protobyte';
+$MESS['PROTOBYTE_CITYSEO_PARTNER_URI'] = 'https://protobyte.example';
+$MESS['PROTOBYTE_CITYSEO_INSTALL_COMPLETE'] = 'Модуль успешно установлен. Перейдите в настройки модуля и укажите сопоставление разделов с HL-блоками.';
+$MESS['PROTOBYTE_CITYSEO_UNINSTALL_COMPLETE'] = 'Модуль успешно удалён. Настройки сохранены.';
